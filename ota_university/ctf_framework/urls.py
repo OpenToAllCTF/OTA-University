2091340731b1d4fa65fef67af2fd257cd943f492
Brandon <reznok@gmail.com>
2018-01-11 18:54:53 -0800

    Added flag submission on challenge pages

--- ota_university/ctf_framework/urls.py
+++ ota_university/ctf_framework/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 
 from .views import *
 
--- ota_university/ctf_framework/views.py
+++ ota_university/ctf_framework/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.contrib.auth import logout as django_logout
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from .models import UserProfile, Challenge
 
 
@@ -30,12 +31,29 @@ def profile(request, user_id):
 
 def challenge(request, challenge_id):
     user = UserProfile.objects.get(user=request.user)
+    message = None
+
+    if request.method == "POST":
+        flag = request.POST["flag"]
+        try:
+            # Check for matching challenge with this flag
+            challenge = Challenge.objects.get(id=challenge_id, flag=flag)
+
+            # Add this challenge to user's completed challenges
+            user.challenges.add(challenge)
+            user.save()
+            message = "Correct!"
+
+        except ObjectDoesNotExist:
+            message = "Incorrect!"
+
     challenge = Challenge.objects.get(id=challenge_id)
 
     context = {
         "challenge": challenge,
         "user": user,
-        "challenge_completed": challenge in user.challenges.all()
+        "challenge_completed": challenge in user.challenges.all(),
+        "message": message
     }
 
     return render(request, "challenge.html", context)
