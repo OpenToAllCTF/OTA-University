c9ad5a9d7bd22d059309f5b1e65c972f657bba24
Forbes, Brandon <reznok@gmail.com>
2018-06-06 15:20:59 -0700

    Require staff for uploads

--- ctf_framework/views/upload.py
+++ ctf_framework/views/upload.py
@@ -1,8 +1,14 @@
 from .base_view import *
 from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def index(request):
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
