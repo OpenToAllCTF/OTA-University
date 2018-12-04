509b98ad21df4de40871a10d4de52cbcba738428
Corb3nik <ian@subnet192.com>
2018-12-04 13:38:05 -0500

    Removed admin.py since we aren't using the django admin page anymore

--- ctf_framework/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Title, Challenge, Category, Writeup
-
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Title)
-admin.site.register(Challenge)
-admin.site.register(Category)
-admin.site.register(Writeup)
