58f57ae9280e5a56f7cbcb179e4e97d5b20c7720
uafio <tschoranov@gmail.com>
2018-12-05 20:19:43 -0800

    [WIP] data for plotting solves
    Data contains solves per category per day for the last 7 days

--- ctf_framework/views/analytics.py
+++ ctf_framework/views/analytics.py
@@ -4,12 +4,41 @@ from .base_view import *
 from django.urls import reverse
 from django.http import HttpResponseForbidden, HttpResponseNotAllowed
 from ..models import Solve
+from django.utils import timezone
+from datetime import timedelta
 
 @login_required()
 def index(request):
     """Get some solves metrics."""
 
-    solves = Solve.objects.all()
+    solves = Solve.objects.prefetch_related('challenge', 'user')
 
-    context = {"solves": solves}
+    solves_chart_data = {}
+
+    for category in ['Misc', 'Web', 'Pwn', 'Forensics', 'Reverse Engineering', 'Crypto']:
+        if category not in solves_chart_data:
+            solves_chart_data[category] = []
+
+        # 1 day ago
+        delta_a = timezone.now() - timedelta(days=1)
+        solves_chart_data[category].append(
+            solves.filter(date__gte=delta_a, challenge__category__name=category).count() )
+
+        # 2 days before that
+        delta_b = delta_a - timedelta(days=1)
+        solves_chart_data[category].append(
+            solves.filter(date__gte=delta_b, date__lt=delta_a, challenge__category__name=category).count() )
+
+        # 5 days before that 
+        for _ in range(5):
+            delta_a = delta_b
+            delta_b = delta_a - timedelta(days=1)
+            solves_chart_data[category].append(
+                solves.filter(date__gte=delta_b, date__lt=delta_a, challenge__category__name=category).count() )
+
+    context = {
+        "solves": solves,
+        # "chart_data": solves_chart_data
+    }
+    
     return render(request, "analytics/index.html", context)
