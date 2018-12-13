7956f1b5b484785f516e54a6099e21c2885c6ac6
Corb3nik <ian@subnet192.com>
2018-12-12 23:21:43 -0500

    Closes #70

--- ctf_framework/templatetags/custom_tags.py
+++ ctf_framework/templatetags/custom_tags.py
@@ -1,3 +1,10 @@
 from django import template
 
 register = template.Library()
+
+def kebab(value):
+    """Replaces spaces with dashes."""
+    return value.replace(' ', '-')
+
+register.filter('kebab', kebab)
+
