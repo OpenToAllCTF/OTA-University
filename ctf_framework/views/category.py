4b314a83abed5386f9bccadd98da7f14d3fa2cd3
Corb3nik <ian@subnet192.com>
2018-12-15 15:41:05 -0500

    Add permissions for category management

--- ctf_framework/views/category.py
+++ ctf_framework/views/category.py
@@ -1,15 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .base_view import *
+from rules.contrib.views import permission_required
 from django.urls import reverse
 from django.http import HttpResponseForbidden, HttpResponseNotAllowed
 
 
 @login_required()
+@permission_required('can_manage_categories')
 def index(request):
     """List all active Categories."""
-    if not request.user.is_staff:
-        return HttpResponseForbidden()
 
     categories = [category for category in Category.objects.all() if not category.parent]
     context = { "categories": categories }
@@ -18,11 +18,9 @@ def index(request):
 
 
 @login_required()
+@permission_required('can_manage_categories')
 def new(request):
     """Create a new category."""
-
-    if not request.user.is_staff:
-        return HttpResponseForbidden()
 
     context = {
         'form': CategoryForm(),
@@ -33,11 +31,9 @@ def new(request):
 
 
 @login_required()
+@permission_required('can_manage_categories')
 def create(request):
     """Save new category."""
-
-    if not request.user.is_staff:
-        return HttpResponseForbidden()
 
     if request.method not in "POST":
         return HttpResponseNotAllowed(permitted_methods=["POST"])
@@ -52,11 +48,9 @@ def create(request):
 
 
 @login_required()
+@permission_required('can_manage_categories')
 def edit(request, category_id):
     """Edit an existing category."""
-
-    if not request.user.is_staff:
-        return HttpResponseForbidden()
 
     try:
         category = Category.objects.get(id=category_id)
@@ -73,11 +67,9 @@ def edit(request, category_id):
 
 
 @login_required()
+@permission_required('can_manage_categories')
 def update(request, category_id):
     """Update existing challenge."""
-
-    if not request.user.is_staff:
-        return HttpResponseForbidden()
 
     if request.method not in "POST":
         return HttpResponseNotAllowed(permitted_methods=["POST"])
