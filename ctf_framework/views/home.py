935bb642c9bd51d25bebf7f3c40db50777eb79a4
Corb3nik <ian@subnet192.com>
2018-12-22 03:41:54 -0500

    Add rankings

--- ctf_framework/views/home.py
+++ ctf_framework/views/home.py
@@ -8,6 +8,22 @@ def index(request):
 
     # Order users by score and last_solve_time
     sorted_users = sorted(users, key=lambda u: (-u.score, u.last_solve_time))
-    context = { "users": sorted_users }
+
+    rankings = []
+    current_rank = 0
+    rankings_to_skip = 1
+    previous_score = None
+    for user in sorted_users:
+
+        if user.score != previous_score:
+            previous_score = user.score
+            current_rank += rankings_to_skip
+            rankings_to_skip = 1
+        else:
+            rankings_to_skip += 1
+
+        rankings.append(current_rank)
+
+    context = { "users": zip(rankings, sorted_users) }
 
     return render(request, "misc/home.html", context=context)
